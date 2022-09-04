93016530d117a74b2f68bf46ced1677977b6ba4f
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-04 07:18:51 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,23 @@
+const counter = {
+    value: 0,
+    increment() {
+        this.value +=1;
+    },
+    decrement() {
+        this.value -= 1;
+    },
+}
+const counters = document.getElementById('counter');
+
+const decrementBtn = counters.getAttribute("decrement");
+const incrementBtn = counters.getAttribute("increment");
+const valueEl = document.getElementById('value');
+
+decrementBtn.addEventListener('click', () => {
+    counter.decrement();
+    valueEl.textContent = counter.value;
+});
+incrementBtn.addEventListener('click', () => {
+    counter.increment();
+    valueEl.textContent = counter.value;
+});
