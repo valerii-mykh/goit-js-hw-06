677aa0f5958261e9e8868a9252fcc04dca62c043
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-07 21:16:47 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,17 @@
-const elements = {
-    email: document.getElementsByClassName('email'),
-    password: document.getElementsByClassName('password'),
-}
-elements.email.addEventListener('submit', buttonClick);
-function buttonClick(event) {
-    if (!email.value) {
-        alert ('Всі поля повинні бути заповнені');
-       
-    };
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.name + " " + formElements.email.value;
+    const password = formElements.password.name + " " + formElements.password.value;
+    if (!formElements.password.value) {
+        alert ('всі поля повинні бути заповнені.')
+    } else{
+        console.log(mail);
+        console.log(password);
+        form.reset()
+    }
 };
