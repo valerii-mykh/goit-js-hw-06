73cc56b8757019218c73ed9b4f109577f7b31977
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-07 22:37:35 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+button.addEventListener('click', () => {
+  let hexColor = [getRandomHexColor()];
+document.body.style.backgroundColor = hexColor;
+color.textContent = hexColor;
+});
