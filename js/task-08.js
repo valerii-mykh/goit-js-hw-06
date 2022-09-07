3d2f45e603126fa9099bec2e0815f2b2ac3be494
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-07 19:45:47 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,7 +2,7 @@ const elements = {
     email: document.getElementsByClassName('email'),
     password: document.getElementsByClassName('password'),
 }
-elements.email.addEventListener ('submit', buttonClick);
+elements.email.addEventListener('submit', buttonClick);
 function buttonClick(event) {
     if (!email.value) {
         alert ('Всі поля повинні бути заповнені');
